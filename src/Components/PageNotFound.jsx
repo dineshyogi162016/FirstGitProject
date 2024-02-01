e2cd720523391dcf5463dcc798f5661d45e57c41
dineshyogi162016 <dineshyogi162016@MyGit>
2024-02-01 14:39:39 +0530

    Token DONE all response send using backend

---
+++ src/Components/PageNotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <>
+      <div class="page404">
+         <h1>404</h1>
+         <h3>Page not found</h3>
+      </div>
+    </>
+  )
+}
+
+export default PageNotFound
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -18,7 +18,6 @@ const Signup = () => {
       }
    });
    const [error, seterror] = useState({})
-   const [signupdata, setsignupdata] = useState([])
    const [passwordshow, setpasswordshow] = useState("password")
    const emailregex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
    const passwordregex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
@@ -38,36 +37,51 @@ const Signup = () => {
 
    const handlesubmit = async()=>{
       if(varify()){
-
-         // Signup Code using localStorage 
-         // let presignupdata = ([...signupdata]);
-         // let signupdataall = presignupdata.concat(data);
-         // setsignupdata(signupdataall)
-
-         // localStorage.setItem("SignupData", JSON.stringify(signupdataall))
-         // setdata({name:"",email:"", password:""})
-         //    navigate("/")
-         //    alert("SignUp Successfully")
-
-         const checksignup = signupdata.find(e => e.userDetails.email === data.userDetails.email) || {};
-         const checksignup1 = Object.entries(checksignup).length;
-
-         console.log("Check data signup data", checksignup1)
-
-      if(checksignup1 <= 0){
          try {
             const response = await fetch(`${process.env.REACT_APP_API_URL}signup`, {
                method: "POST",
                headers: {
-                  'Content-Type' : 'application/json',
+                  'Content-Type' : "application/json",
                },
-               body: JSON.stringify(data),
-            })
+               body: JSON.stringify(data)
+            }) 
 
             const result = await response.json();
 
-            setdata({name:"",email:"", password:""})
+            if(result){
+               // Sweet Alert use 
+               const Toast = Swal.mixin({
+                  toast: true,
+                  position: "top-end",
+                  showConfirmButton: false,
+                  timer: 2000,
+                  timerProgressBar: true,
+                  didOpen: (toast) => {
+                  toast.onmouseenter = Swal.stopTimer;
+                  toast.onmouseleave = Swal.resumeTimer;
+                  }
+               });
 
+               if(result.massage){
+                  Toast.fire({
+                     icon: "warning",
+                     title: result.massage
+                  }); 
+               }else{
+                  Toast.fire({
+                     icon: "success",
+                     title: "Successfully Signup "
+                  });
+
+                  setTimeout(() => {
+                     navigate("/")
+                  }, 1500);
+               
+               }
+
+            }
+
+         } catch (error) {
             // Sweet Alert use 
             const Toast = Swal.mixin({
                toast: true,
@@ -76,42 +90,17 @@ const Signup = () => {
                timer: 2000,
                timerProgressBar: true,
                didOpen: (toast) => {
-                 toast.onmouseenter = Swal.stopTimer;
-                 toast.onmouseleave = Swal.resumeTimer;
+               toast.onmouseenter = Swal.stopTimer;
+               toast.onmouseleave = Swal.resumeTimer;
                }
-             });
-             Toast.fire({
-               icon: "success",
-               title: "Successfully Signup "
-             });
+            });
+            Toast.fire({
+               icon: "warning",
+               title: "Something went wrong!"
+            });
 
-             setTimeout(() => {
-               navigate("/")
-            }, 2000);
-
-         } catch (error) {
-            console.log("Error in Signup.jsx: ", error)
+            console.log("Error Login : ", error )
          }
-      }else if (checksignup1 > 0){
-          // Sweet Alert use 
-         const Toast = Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "warning",
-            title: "User is already exists"
-          });
-      }
-
-
       }
    }
 
@@ -149,27 +138,6 @@ const Signup = () => {
       return valid;
    }
 
-   const getRegisterUsers = async() => {
-      try {
-         let response = await fetch(`${process.env.REACT_APP_API_URL}SignupDetails`);
-         let result = await response.json();
-
-         setsignupdata(result)
-         // console.log("all Signup users: ",result)
-
-      } catch (error) {
-         console.log("All register users Error: ", error )
-      }
-   }
-
-
-   useEffect(()=> {
-      let getdata = JSON.parse(localStorage.getItem("SignupData"))|| [];
-      setsignupdata(getdata);      
-
-      getRegisterUsers()
-      // console.log("Get All a Data: ", getdata)
-   },[]);
 
   return (
     <>
